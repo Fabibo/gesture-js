1f6c202e69739e0461d235e81ed1301f7294d140
Fabian Bohr <fabian@bohr.eu>
2015-06-29 22:35:33 +0200

    some more changes

--- js/gesture.js
+++ js/gesture.js
@@ -1,9 +1,25 @@
 /**
  * Created by Fabian on 29.06.2015.
  */
+
 function init() {
     var canvas = document.getElementById("canvas_1");
+    var width = window.innerWidth;
+
+    var height = window.innerHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+    if (canvas && canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        if (ctx) {
+            ctx.strokeStyle = "red";
+            ctx.lineWidth = 10;
+            ctx.strokeRect(0,0,canvas.width, canvas.height);
+        }
+    }
     canvas.addEventListener("mousedown", doTouchStart, false);
+    canvas.addEventListener("touchstart", doTouchStart, false);
 }
 
 function doTouchStart(event) {
@@ -18,6 +34,18 @@ function doTouchStart(event) {
         canvas_y = event.targetTouches[0].pageY;
     }
 
-    alert("X=" + canvas_x + " Y=" + canvas_y);
-    //alert("List Size: event.targetTouches");
+    var canvas = document.getElementById("canvas_1");
+    if (canvas && canvas.getContext) {
+        var ctx = canvas.getContext("2d");
+        if (ctx) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            ctx.strokeStyle = "red";
+            ctx.lineWidth = 10;
+            ctx.strokeRect(0,0,canvas.width, canvas.height);
+            ctx.font="30px Verdana";
+            ctx.fillStyle = "#000000";
+            ctx.fillText("X= " + canvas_x,10,50);
+            ctx.fillText("Y= " + canvas_y,10,90);
+        }
+    }
 }
